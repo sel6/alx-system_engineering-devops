f7be98bb6fae80693a6b1966921abf50fb90851f
Selam <84750917+sel6@users.noreply.github.com>
2021-11-02 14:45:28 +0300

    Update 2-repetition_token_1.rb

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,4 +1,5 @@
 #!/bin/env ruby
 # only matches one or no amount of b in between h and tn
+# '?' 0 or one amount of the char before it
 
-puts ARGV[0].scan(/hb{,1}tn/).join
+puts ARGV[0].scan(/hb?tn/).join
