79f46767cc2510c9188e02fbecb92a37e7ecf348
Selam <84750917+sel6@users.noreply.github.com>
2021-11-02 15:29:33 +0300

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -3,7 +3,8 @@
 #'\'before'[' should be used so it won't take [ as regex expression but part of the string
 #'()' captures everything enclosed
 #'.' any single characters but when it is followed by * countless single characters will follow
-# to limit the amount of single characters enclosed we use '?' quantifier. It limits after *, +, ?, or {} quantifers preceed it
+# to limit the amount of single characters enclosed we use '?' quantifier
+# '?' limits after *, +, ?, or {} quantifers, it makes it non-greedy, stops when it finds match
 # ',' joins the three groups with comma
 
 puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
