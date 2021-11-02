77a8e9b700d3d62dbe401a06a3ccd6daf75b56eb
Selam <84750917+sel6@users.noreply.github.com>
2021-11-02 23:22:55 +0300

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/env ruby
-#sender, receiver, flags
-#'\'before'[' should be used so it won't take [ as regex expression but part of the string
+#'\'before'[]' should be used so it won't take '[]' as part of regex expression but of string
 #'()' captures everything enclosed
-#'.' any single characters but when it is followed by * countless single characters will follow
+#'.' any single characters but when it is followed by '*' all single characters will follow
 # to limit the amount of single characters enclosed we use '?' quantifier
-# '?' limits after *, +, ?, or {} quantifers, it makes it non-greedy, stops when it finds match
+# '?' limits after *, +, ?, or {} quantifers, it makes them non-greedy
+# since they match maximum amount possible by default.
+# '?'  stops when it finds match, minimum amount possible.
 # ',' joins the three groups with comma
+#groups of [SENDER, RECIVER, FLAGS]
 
 puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
