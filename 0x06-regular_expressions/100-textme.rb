65bd702ca7c8b728c343f23840a0c03020cc2252
Selam <84750917+sel6@users.noreply.github.com>
2021-11-02 15:18:43 +0300

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,8 @@
 #!/usr/bin/env ruby
 #sender, receiver, flags
+#'\'before'[' should be used so it won't take [ as regex expression but part of the string
+#'()' captures everything enclosed
+#'.' any single characters but when it is followed by * countless single characters will follow
+# to limit the amount of single characters enclosed we use '?' quantifier. It limits after *, +, ?, or {} quantifers preceed it
 
-puts ARGV[0].scan(/\d\D/).join
+puts ARGV[0].scan(\[from:(.*?)\]\[to:(.*?)\]\[flags:(.*?)\]).join
