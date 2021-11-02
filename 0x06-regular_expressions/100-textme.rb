6b5f5d0fd9ea55d6f25569806a0468901e96b26a
Selam <84750917+sel6@users.noreply.github.com>
2021-11-02 15:20:59 +0300

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -4,5 +4,6 @@
 #'()' captures everything enclosed
 #'.' any single characters but when it is followed by * countless single characters will follow
 # to limit the amount of single characters enclosed we use '?' quantifier. It limits after *, +, ?, or {} quantifers preceed it
+# ',' joins the three groups with comma
 
-puts ARGV[0].scan(\[from:(.*?)\]\[to:(.*?)\]\[flags:(.*?)\]).join(,)
+puts ARGV[0].scan(\[from:(.*?)\]\[to:(.*?)\]\[flags:(.*?)\]).join(",")
