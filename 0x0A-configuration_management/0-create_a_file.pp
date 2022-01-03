82577cb80d9644a1fce89f9bf7bf8cd5ff9a3ab3
sel6 <kabodshekinah@gmail.com>
2022-01-03 10:43:15 +0000

    create file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#Create file holberton
+file { 'holberton':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/holberton',
+}
